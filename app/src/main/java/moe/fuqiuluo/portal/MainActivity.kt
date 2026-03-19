b0d016ddd8b5923971e01750dab353bde3c2a338
Patr1ck Shaw <133486225+Patr1ck-74@users.noreply.github.com>
2026-03-19 17:17:40 +0800

    Update MainActivity.kt

--- app/src/main/java/moe/fuqiuluo/portal/MainActivity.kt
+++ app/src/main/java/moe/fuqiuluo/portal/MainActivity.kt
@@ -390,7 +390,7 @@ class MainActivity : AppCompatActivity() {
                 try {
                     mSuggestionSearch!!.requestSuggestion(SuggestionSearchOption()
                         .keyword(query)
-                        .city(mCityString ?: "北京市")
+                        .city(if (mCityString.isNullOrEmpty()) "北京市" else mCityString)
                     )
 
                     baiduMapViewModel.baiduMap.clear()
@@ -408,7 +408,7 @@ class MainActivity : AppCompatActivity() {
                         mSuggestionSearch!!.requestSuggestion(
                             SuggestionSearchOption()
                                 .keyword(newText)
-                                .city(mCityString ?: "北京市")
+                                .city(if (mCityString.isNullOrEmpty()) "北京市" else mCityString)
                         )
                     } catch (e: Exception) {
                         Toast.makeText(this@MainActivity, "搜索出错", Toast.LENGTH_SHORT).show()
